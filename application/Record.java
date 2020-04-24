c644ec22e1ed3ac2df7b7f4f0ed515d4185cd2bc
hshen20 <61573136+hshen20@users.noreply.github.com>
2020-04-24 18:56:31 -0500

    Add files via upload

--- application/Record.java
+++ application/Record.java
@@ -1,8 +1,9 @@
 /**
  * Record.java created by aTeam 147 in Proudly_Cheese project
  * 
- * Author: Hairong Yin (hyin55@wisc.edu) (Lec 002), Haonan Shen (hshen37@wisc.edu) (Lec 001), 
- * Xiaoxi Sun (xsun279@wisc.edu) (Lec 002), Zhiwei Song (zsong96@wisc.edu) (Lec 002)
+ * Author: Hairong Yin (hyin55@wisc.edu) (Lec 002), Haonan Shen
+ * (hshen37@wisc.edu) (Lec 001), Xiaoxi Sun (xsun279@wisc.edu) (Lec 002), Zhiwei
+ * Song (zsong96@wisc.edu) (Lec 002)
  * 
  * Date: 04/19/2020
  * 
@@ -25,81 +26,83 @@ import java.util.Date;
  * @author aTeam 147 (2020)
  *
  */
-public class Record implements Comparable<Record>{
-  private Date date; // date of the record
-  private String farmID; // denotes the farm of this record 
-  private int weight; // the weight of milk provided by the farm specified in ID
+public class Record implements Comparable<Record> {
+	private Date date; // date of the record
+	private String farmID; // denotes the farm of this record
+	private int weight; // the weight of milk provided by the farm specified in ID
 
-  /**
-   * Constructor of the record
-   * 
-   * @param date - of the record
-   * @param farmID - denotes the farm of this record
-   * @param weight - the weight of milk provided by the farm specified in ID
-   */
-  public Record(Date date, String farmID, int weight) {
-    this.date = date;
-    this.farmID = farmID;
-    this.weight = weight;
-  }
-  
-  /**
-   * @return the date
-   */
-  public Date getDate() {
-    return date;
-  }
+	/**
+	 * Constructor of the record
+	 * 
+	 * @param date   - of the record
+	 * @param farmID - denotes the farm of this record
+	 * @param weight - the weight of milk provided by the farm specified in ID
+	 */
+	public Record(Date date, String farmID, int weight) {
+		this.date = date;
+		this.farmID = farmID;
+		this.weight = weight;
+	}
 
-  /**
-   * @param date the date to set
-   */
-  public void setDate(Date date) {
-    this.date = date;
-  }
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
 
-  /**
-   * @return the farmID
-   */
-  public String getFarmID() {
-    return farmID;
-  }
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-  /**
-   * @param farmID the farmID to set
-   */
-  public void setFarmID(String farmID) {
-    this.farmID = farmID;
-  }
+	/**
+	 * @return the farmID
+	 */
+	public String getFarmID() {
+		return farmID;
+	}
 
-  /**
-   * @return the weight
-   */
-  public int getWeight() {
-    return weight;
-  }
+	/**
+	 * @param farmID the farmID to set
+	 */
+	public void setFarmID(String farmID) {
+		this.farmID = farmID;
+	}
 
-  /**
-   * @param weight the weight to set
-   */
-  public void setWeight(int weight) {
-    this.weight = weight;
-  }
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
 
-  /**
-   * Compare method. Compare priority is: farmID, date, and weight.
-   * 
-   * @param Record r - the record to be compared with
-   * @return a int value denote compare result. If greater than 0, this > r.
-   *    If equal to 0, this = r. If less than 0, this < r.
-   */
-  @Override
-  public int compareTo(Record r) {
-    if (farmID.compareTo(r.farmID) != 0) {
-      return farmID.compareTo(r.farmID);
-    } else if (date.compareTo(r.date) != 0) {
-      return date.compareTo(r.date);
-    } else {
-      return weight - r.weight;
-    }
-  }
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * Compare method. Compare priority is: farmID, date, and weight.
+	 * 
+	 * @param Record r - the record to be compared with
+	 * @return a int value denote compare result. If greater than 0, this > r. If
+	 *         equal to 0, this = r. If less than 0, this < r.
+	 */
+	@Override
+	public int compareTo(Record r) {
+		int farmIDDiff = farmID.compareTo(r.farmID);
+		if (farmIDDiff != 0)
+			return farmIDDiff;
+		int dateDiff = date.compareTo(r.date);
+		if (dateDiff != 0)
+			return dateDiff;
+		else
+			return weight - r.weight;
+
+	}
 }
